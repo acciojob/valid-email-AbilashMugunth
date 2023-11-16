0eb58fb2405f452734ed4ee0513c708d5bcee4a4
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-16 09:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function validEmail(str) {
-  //your JS code here.
+function validEmail(email) {
+    if (
+    email == "" ||
+    email == null ||
+    email.endsWith(".") ||
+    email.indexOf("@") == -1
+  ) {
+    return false;
+  }
+
+  let splitted = email.split("@");
+  console.log(splitted);
+  if (splitted[0].length == 0 || splitted[1].length == 0) {
+    return false;
+  }
 }
 
 // Do not change the code below.
